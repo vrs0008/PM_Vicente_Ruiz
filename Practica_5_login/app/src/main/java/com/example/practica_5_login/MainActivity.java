09bd386496105ee04e221b4fc6059ad449e5430e
Vicente Ruiz <vrs0008@alu.medac.es>
2025-11-10 17:57:56 +0100

    PM_Tema5_Tarea1_Ejercicio1
    PM_Tema5_Tarea1_Ejercicio1

--- Practica_5_login/app/src/main/java/com/example/practica_5_login/MainActivity.java
+++ Practica_5_login/app/src/main/java/com/example/practica_5_login/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.practica_5_login;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.stream.Stream;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText tCorreo;
+    private EditText tPass;
+    private Button bContinuar;
+    private Switch sRecordar;
+    TextView txMensaje;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,62 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tCorreo = findViewById(R.id.tCorreo);
+        tPass = findViewById(R.id.tPass);
+        bContinuar = findViewById(R.id.bContinuar);
+        sRecordar = findViewById(R.id.sRecordar);
+        txMensaje = findViewById(R.id.txMensaje);
+
+
+        bContinuar.setOnClickListener(v -> {
+
+            String correo = tCorreo.getText().toString();
+            String pass = tPass.getText().toString();
+
+            String correoGuardado;
+            String passGuardada;
+
+            boolean recordar = sRecordar.isChecked();
+
+            if (recordar) {
+                correoGuardado = tCorreo.getText().toString();
+                passGuardada = tPass.getText().toString();
+
+                if (correoGuardado.isEmpty() || passGuardada.isEmpty()) {
+                    txMensaje.setText("El campo del correo o de la contraseña está vacio");
+                    txMensaje.setTextColor(Color.RED);
+
+                } else {
+
+                    //correoGuardado = tCorreo.getText().toString();
+                    //passGuardada = tPass.getText().toString();
+                    if (correo.equals(correoGuardado) && pass.equals(passGuardada)) {
+                        txMensaje.setText("Bienvenido");
+                        txMensaje.setTextColor(Color.GREEN);
+                        tCorreo.setText("");
+                        tPass.setText("");
+                    } else {
+                        txMensaje.setText("Correo y/o contraseña incorrectos");
+                        txMensaje.setTextColor(Color.RED);
+                    }
+                }
+
+            } else if (correo.isEmpty() || pass.isEmpty()) {
+                txMensaje.setText("El campo del correo o de la contraseña está vacio");
+                txMensaje.setTextColor(Color.RED);
+
+            } else if (correo.equals("correo@correo.com") && pass.equals("123")) {
+                txMensaje.setText("Bienvenido");
+                txMensaje.setTextColor(Color.GREEN);
+            } else {
+                txMensaje.setText("Correo y/o contraseña incorrectos");
+                txMensaje.setTextColor(Color.RED);
+            }
+
+
+        });
+
+
     }
 }
